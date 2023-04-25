a37943dd00267f6150f5f53df668c839c0593bfc
MtJakhar <thvsd.10fingers@gmail.com>
2023-04-25 13:57:31 -0400

    added layout to restaurant

--- app/restaurant/[slug]/menu/page.tsx
+++ app/restaurant/[slug]/menu/page.tsx
@@ -1,19 +1,12 @@
-import NavBar from '../../../components/NavBar'
-import Header from '../components/Header'
 import RestaurantNavBar from '../components/RestaurantNavBar'
 import Menu from '../components/Menu'
 
 
 export default function RestaurantMenu(){
   return (
-    <>
-      <Header />
-      <div className="flex m-auto w-2/3 justify-between items-start 0 -mt-11">
-        <div className="bg-white w-[100%] rounded p-3 shadow">
-          <RestaurantNavBar />
-          <Menu />
-        </div>
-      </div>
-    </>
+    <div className="bg-white w-[100%] rounded p-3 shadow">
+      <RestaurantNavBar />
+      <Menu />
+    </div>
   )
 }
--- app/restaurant/[slug]/page.tsx
+++ app/restaurant/[slug]/page.tsx
@@ -1,6 +1,3 @@
-import Link from 'next/link'
-import NavBar from '../../components/NavBar'
-import Header from './components/Header'
 import RestaurantNavBar from './components/RestaurantNavBar'
 import Title from './components/Title'
 import Rating from './components/Rating'
@@ -12,19 +9,16 @@ import ReservationCard from './components/ReservationCard'
 export default function RestaurantDetails(){
   return (
   <>
-    <Header />
-    <div className="flex m-auto w-2/3 justify-between items-start 0 -mt-11">
-      <div className="bg-white w-[70%] rounded p-3 shadow">
-        <RestaurantNavBar /> 
-        <Title />
-        <Rating />
-        <Description />
-        <Images />
-        <Reviews />
-      </div>
-      <div className="w-[27%] relative text-reg">
-        <ReservationCard />
-      </div>
+    <div className="bg-white w-[70%] rounded p-3 shadow">
+      <RestaurantNavBar /> 
+      <Title />
+      <Rating />
+      <Description />
+      <Images />
+      <Reviews />
+    </div>
+    <div className="w-[27%] relative text-reg">
+      <ReservationCard />
     </div>
   </>
   )
