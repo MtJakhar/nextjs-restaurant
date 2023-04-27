e684c50f29d981826506c67442fd43b22c81aac4
MtJakhar <thvsd.10fingers@gmail.com>
2023-04-26 20:39:54 -0400

    added menu and dynamic overview

--- app/restaurant/[slug]/menu/page.tsx
+++ app/restaurant/[slug]/menu/page.tsx
@@ -1,12 +1,37 @@
-import RestaurantNavBar from '../components/RestaurantNavBar'
-import Menu from '../components/Menu'
+import RestaurantNavBar from "../components/RestaurantNavBar";
+import Menu from "../components/Menu";
+import { PrismaClient } from "@prisma/client";
 
+const prisma = new PrismaClient();
 
-export default function RestaurantMenu(){
-  return (
-    <div className="bg-white w-[100%] rounded p-3 shadow">
-      <RestaurantNavBar />
-      <Menu />
-    </div>
-  )
+const fetchRestaurantMenu = async (slug: string) => {
+    const restaurant = await prisma.restaurant.findUnique({
+        where: {
+            slug,
+        },
+        select: {
+            items: true,
+        },
+    });
+
+    if (!restaurant) {
+        throw new Error();
+    }
+
+    return restaurant.items;
+};
+
+export default async function RestaurantMenu({
+    params,
+}: {
+    params: { slug: string };
+}) {
+    const menu = await fetchRestaurantMenu(params.slug);
+    console.log({menu})
+    return (
+        <div className="bg-white w-[100%] rounded p-3 shadow">
+            <RestaurantNavBar slug={params.slug} />
+            <Menu menu={menu} />
+        </div>
+    );
 }
--- app/restaurant/[slug]/page.tsx
+++ app/restaurant/[slug]/page.tsx
@@ -5,16 +5,49 @@ import Description from './components/Description'
 import Images from './components/Images'
 import Reviews from './components/Reviews'
 import ReservationCard from './components/ReservationCard'
+import { PrismaClient } from '@prisma/client'
 
-export default function RestaurantDetails(){
+const prisma = new PrismaClient();
+
+interface Restaurant {
+  id: number;
+  name: string;
+  images: string[];
+  description: string;
+  slug: string;
+}
+
+const fetchRestaurantBySlug = async (slug: string) => {
+  const restaurant = await prisma.restaurant.findUnique({
+    where: {
+      slug
+    },
+    select: {
+      id: true,
+      name: true,
+      images: true,
+      description: true,
+      slug: true
+    }
+  });
+
+  if(!restaurant){
+    throw new Error();
+  }
+
+  return restaurant;
+}
+
+export default async function RestaurantDetails({params}: {params: {slug: string}}){
+  const restaurant = await fetchRestaurantBySlug(params.slug)
   return (
   <>
     <div className="bg-white w-[70%] rounded p-3 shadow">
-      <RestaurantNavBar /> 
-      <Title />
+      <RestaurantNavBar slug={restaurant.slug}/> 
+      <Title name={restaurant.name}/>
       <Rating />
-      <Description />
-      <Images />
+      <Description description={restaurant.description}/>
+      <Images images={restaurant.images}/>
       <Reviews />
     </div>
     <div className="w-[27%] relative text-reg">
