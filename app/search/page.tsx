00523c2cde384f6146c527e018b68308a39e0b81
MtJakhar <thvsd.10fingers@gmail.com>
2023-04-27 18:31:23 -0400

    added search ability not complete yet

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,19 +1,58 @@
-import Link from 'next/link'
-import NavBar from '../components/NavBar'
-import Header from './components/Header'
-import SearchSideBar from './components/SearchSideBar'
-import RestaurantCard from './components/RestaurantCard'
+import Header from "./components/Header";
+import SearchSideBar from "./components/SearchSideBar";
+import RestaurantCard from "./components/RestaurantCard";
+import { PrismaClient } from "@prisma/client";
 
-export default function Search(){
-  return (
-    <>
-      <Header />
-      <div className="flex py-4 m-auto w-2/3 justify-between items-start">
-        <SearchSideBar />
-        <div className="w-5/6">
-          <RestaurantCard />
-        </div>
-      </div>
-    </>
-  )
+const prisma = new PrismaClient();
+
+const fetchRestaurantsByCity = (city: string | undefined) => {
+  const select = {
+    id: true,
+    name: true,
+    main_image: true,
+    price: true,
+    cuisine: true,
+    location: true,
+    slug: true
+  }
+
+  if (!city) return prisma.restaurant.findMany(select);
+
+  return prisma.restaurant.findMany({
+      where: {
+          location: {
+              name: {
+                  equals: city.toLowerCase(),
+              },
+          },
+      },
+      select,
+  });
+};
+
+export default async function Search({
+    searchParams,
+}: {
+    searchParams: { city: string };
+}) {
+    const restaurants = await fetchRestaurantsByCity(searchParams.city);
+
+    console.log({ restaurants });
+    return (
+        <>
+          <Header />
+          <div className="flex py-4 m-auto w-2/3 justify-between items-start">
+              <SearchSideBar />
+              <div className="w-5/6">
+                {restaurants.length ? (
+                <>
+                  {restaurants.map(restaurant => (
+                    <RestaurantCard restaurant={restaurant}/>
+                    ))}
+                </>
+                ): <p>Sorry, we found no restaurants in this area</p>}
+              </div>
+          </div>
+        </>
+    );
 }
