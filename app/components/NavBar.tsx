e56660c98de36cebf855318bc93a6a73f97fd6b7
MtJakhar <thvsd.10fingers@gmail.com>
2023-05-04 10:52:43 -0400

    added signIn signUp buttons

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+import LoginModal from './LoginModal'
 
 export default function NavBar() {
   return (
@@ -8,12 +9,8 @@ export default function NavBar() {
     </Link>
     <div>
       <div className="flex">
-        <button
-          className="bg-blue-400 text-white border p-1 px-4 rounded mr-3"
-        >
-          Sign in
-        </button>
-        <button className="border p-1 px-4 rounded">Sign up</button>
+        <LoginModal isSignin={true}/>
+        <LoginModal isSignin={false}/>
       </div>
     </div>
   </nav>
