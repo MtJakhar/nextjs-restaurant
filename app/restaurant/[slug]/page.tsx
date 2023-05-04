e0dff52051e396942fab365e70372d5cd6eff3eb
MtJakhar <thvsd.10fingers@gmail.com>
2023-05-04 09:43:27 -0400

    created error handling, and loading pages

--- app/restaurant/[slug]/page.tsx
+++ app/restaurant/[slug]/page.tsx
@@ -6,6 +6,7 @@ import Images from './components/Images'
 import Reviews from './components/Reviews'
 import ReservationCard from './components/ReservationCard'
 import { PrismaClient, Review } from '@prisma/client'
+import { notFound } from 'next/navigation'
 
 const prisma = new PrismaClient();
 
@@ -34,7 +35,7 @@ const fetchRestaurantBySlug = async (slug: string): Promise<Restaurant> => {
   });
 
   if(!restaurant){
-    throw new Error();
+    notFound()
   }
 
   return restaurant;
