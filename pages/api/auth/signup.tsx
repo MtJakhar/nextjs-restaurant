3e4a79856dfe884d78292fb15cb89f0f4a07bc6e
MtJakhar <thvsd.10fingers@gmail.com>
2023-05-05 12:45:25 -0400

    validation for user input userauth

--- pages/api/auth/signup.tsx
+++ pages/api/auth/signup.tsx
@@ -1,12 +1,53 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import validator from "validator"
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ){
   if(req.method === "POST"){
+    const { firstName, email, phone, city, password } = req.body;
+
+    const errors: string[] = [];
+
+    const validationSchema = [
+      {
+        valid: validator.isLength(firstName, {
+          min: 1,
+          max: 20,
+        }),
+        errorMessage: "First name is Invalid"
+      },
+      {
+        valid: validator.isEmail(email),
+        errorMessage: "Email is invalid"
+      },
+      {
+        valid: validator.isMobilePhone(phone),
+        errorMessage: "Phone number is invalid"
+      },
+      {
+        valid: validator.isLength(city, { min: 1 }),
+        errorMessage: "Email is invalid"
+      },
+      {
+        valid: validator.isStrongPassword(password),
+        errorMessage: "Password is not strong enough"
+      },
+    ];
+
+    validationSchema.forEach((check) => {
+      if(!check.valid){
+        errors.push(check.errorMessage)
+      }
+    })
+
+    if(errors.length){
+      return res.status(400).json({errorMessage: errors[0]})
+    }
+
     res.status(200).json({
-      hello: "there",
+      hello: "body",
     });
   }
 }
