3c326ccea68b371501d646c39dad8c2fb8299fcf
MtJakhar <thvsd.10fingers@gmail.com>
2023-05-09 14:28:13 -0400

    client side validation

--- app/components/AuthModal.tsx
+++ app/components/AuthModal.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Box from "@mui/material/Box";
 import Modal from "@mui/material/Modal";
 import AuthModalInputs from "./AuthModalInputs";
@@ -32,6 +32,23 @@ export default function AuthModal({ isSignin }: { isSignin: boolean }) {
     city: "",
     password: "",
   });
+
+  const [disabled, setDisabled] = useState(true)
+
+  useEffect(() => {
+    if(isSignin) {
+      if(inputs.password && inputs.email){
+        return setDisabled(false)
+      }
+    } else {
+      if(inputs.firstName && inputs.lastName && inputs.email && inputs.password && inputs && inputs.phone) {
+        return setDisabled(false)
+      }
+    }
+
+    setDisabled(true)
+  }, [inputs])
+  
 
   const handleChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
     setInputs({
@@ -77,7 +94,7 @@ export default function AuthModal({ isSignin }: { isSignin: boolean }) {
               handleChangeInput={handleChangeInput}
               isSignin={isSignin}
             />
-            <button className="uppercase bg-red-600 w-full text-white p-3 rounded text-sm mb-5 disabled:bg-gray-400">
+            <button className="uppercase bg-red-600 w-full text-white p-3 rounded text-sm mb-5 disabled:bg-gray-400" disabled={disabled}>
               {renderContent(
                 "Log Into Your Account",
                 "Create Your OpenTable Account"
