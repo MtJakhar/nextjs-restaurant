c09530187a278c83c8fb01fdc51aadc66411b880
MtJakhar <thvsd.10fingers@gmail.com>
2023-05-11 10:45:15 -0400

    adding circular progress

--- app/components/AuthModal.tsx
+++ app/components/AuthModal.tsx
@@ -4,6 +4,8 @@ import Box from "@mui/material/Box";
 import Modal from "@mui/material/Modal";
 import AuthModalInputs from "./AuthModalInputs";
 import useAuth from "../../hooks/useAuth";
+import { AuthenticationContext } from "../context/AuthContext";
+import CircularProgress from "@mui/material/CircularProgress";
 
 const style = {
   position: "absolute" as "absolute",
@@ -20,7 +22,8 @@ export default function AuthModal({ isSignin }: { isSignin: boolean }) {
   const [open, setOpen] = useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
-  const {signin} = useAuth()
+  const { signin } = useAuth();
+  const { loading, data, error } = useContext(AuthenticationContext);
 
   const renderContent = (signinContent: string, signupContent: string) => {
     return isSignin ? signinContent : signupContent;
@@ -35,28 +38,34 @@ export default function AuthModal({ isSignin }: { isSignin: boolean }) {
     password: "",
   });
 
-  const [disabled, setDisabled] = useState(true)
+  const [disabled, setDisabled] = useState(true);
 
   useEffect(() => {
     if (isSignin) {
-      if(inputs.password && inputs.email){
-        return setDisabled(false)
+      if (inputs.password && inputs.email) {
+        return setDisabled(false);
       }
     } else {
-      if (inputs.firstName && inputs.lastName && inputs.email && inputs.password && inputs && inputs.phone) {
-        return setDisabled(false)
+      if (
+        inputs.firstName &&
+        inputs.lastName &&
+        inputs.email &&
+        inputs.password &&
+        inputs &&
+        inputs.phone
+      ) {
+        return setDisabled(false);
       }
     }
 
-    setDisabled(true)
-  }, [inputs])
+    setDisabled(true);
+  }, [inputs]);
 
   const handleClick = () => {
     if (isSignin) {
-      signin({email: inputs.email, password: inputs.password})
+      signin({ email: inputs.email, password: inputs.password });
     }
-  }
-  
+  };
 
   const handleChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
     setInputs({
@@ -83,32 +92,43 @@ export default function AuthModal({ isSignin }: { isSignin: boolean }) {
         aria-describedby="modal-modal-description"
       >
         <Box sx={style}>
-          <div className="p-2">
-            <div className="uppercase font-bold text-center pb-2 border-b mb-2">
-              <p className="text-sm">
-                {renderContent("Sign In", "Create Account")}
-              </p>
+          {loading ? (
+            <div className="px-2 flex justify-center">
+              <CircularProgress />
             </div>
-          </div>
-          <div className="m-auto">
-            <h2 className="text-2xl font-light text-center">
-              {renderContent(
-                "Log Into Your Account",
-                "Create Your OpenTable Account"
-              )}
-            </h2>
-            <AuthModalInputs
-              inputs={inputs}
-              handleChangeInput={handleChangeInput}
-              isSignin={isSignin}
-            />
-            <button className="uppercase bg-red-600 w-full text-white p-3 rounded text-sm mb-5 disabled:bg-gray-400" disabled={disabled} onClick={handleClick}>
-              {renderContent(
-                "Log Into Your Account",
-                "Create Your OpenTable Account"
-              )}
-            </button>
-          </div>
+          ) : (
+            <div className="p-2">
+              <div className="uppercase font-bold text-center pb-2 border-b mb-2">
+                <p className="text-sm">
+                  {renderContent("Sign In", "Create Account")}
+                </p>
+              </div>
+
+              <div className="m-auto">
+                <h2 className="text-2xl font-light text-center">
+                  {renderContent(
+                    "Log Into Your Account",
+                    "Create Your OpenTable Account"
+                  )}
+                </h2>
+                <AuthModalInputs
+                  inputs={inputs}
+                  handleChangeInput={handleChangeInput}
+                  isSignin={isSignin}
+                />
+                <button
+                  className="uppercase bg-red-600 w-full text-white p-3 rounded text-sm mb-5 disabled:bg-gray-400"
+                  disabled={disabled}
+                  onClick={handleClick}
+                >
+                  {renderContent(
+                    "Log Into Your Account",
+                    "Create Your OpenTable Account"
+                  )}
+                </button>
+              </div>
+            </div>
+          )}
         </Box>
       </Modal>
     </div>
--- hooks/useAuth.ts
+++ hooks/useAuth.ts
@@ -1,23 +1,51 @@
-import axios from "axios"
+import axios from "axios";
+import { useContext } from "react"
+import { AuthenticationContext } from "../app/context/AuthContext";
 
 const useAuth = () => {
-  const signin = async ({email, password}: { email: string, password: string }) => {
+  const { data, error, loading, setAuthState } = useContext(
+    AuthenticationContext
+  );
+
+  const signin = async ({
+    email,
+    password,
+  }: {
+    email: string;
+    password: string;
+  }) => {
     try {
-      const response = await axios.post("http://localhost:3000/api/auth/signin", {
-        email,
-        password
+      setAuthState({
+        data: null,
+        error: null,
+        loading: true
       })
-      console.log(response)
-    } catch (error) {
-      console.log(error)
-    };
-    
-  }
-  const signup = async () => {}
+
+      const response = await axios.post(
+        "http://localhost:3000/api/auth/signin",
+        {
+          email,
+          password,
+        }
+      );
+      setAuthState({
+        data: response.data,
+        error: null,
+        loading: false
+      })
+    } catch (error: any) {
+      setAuthState({
+        data: null,
+        error: error.response.data.errorMessage,
+        loading: false
+      })
+    }
+  };
+  const signup = async () => { };
   return {
     signin,
     signup,
-  }
-}
+  };
+};
 
-export default useAuth
+export default useAuth;
