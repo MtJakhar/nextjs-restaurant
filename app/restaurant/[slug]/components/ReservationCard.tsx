ad72f3f73b0e932f0d12c06245594e37eb36a09c
MtJakhar <thvsd.10fingers@gmail.com>
2023-05-16 13:05:59 -0400

    made time selector work dynamically

--- app/restaurant/[slug]/components/ReservationCard.tsx
+++ app/restaurant/[slug]/components/ReservationCard.tsx
@@ -1,18 +1,42 @@
-"use client"
-import { partySize } from "../../../../data/index"
-import DatePicker from "react-datepicker"
-import { useState } from "react"
+"use client";
+import { partySize, times } from "../../../../data/index";
+import DatePicker from "react-datepicker";
+import { useState } from "react";
 
-export default function ReservationCard() {
-
-  const [selectedDate, setSelectedDate] = useState<Date | null>(new Date())
+export default function ReservationCard({
+  openTime,
+  closeTime,
+}: {
+  openTime: string;
+  closeTime: string;
+}) {
+  const [selectedDate, setSelectedDate] = useState<Date | null>(new Date());
 
   const handleChangeDate = (date: Date | null) => {
-    if(date){
-      return setSelectedDate(date)
+    if (date) {
+      return setSelectedDate(date);
     }
-    return setSelectedDate(null)
-  }
+    return setSelectedDate(null);
+  };
+
+  const filterTimeByRestaurantOpenWindow = () => {
+    const timesWithInWindow: typeof times = [];
+
+    let isWithinWindow = false;
+
+    times.forEach((time) => {
+      if (time.time === openTime) {
+        isWithinWindow = true;
+      }
+      if (isWithinWindow) {
+        timesWithInWindow.push(time);
+      }
+      if (time.time === closeTime) {
+        isWithinWindow = false;
+      }
+    });
+    return timesWithInWindow;
+  };
 
   return (
     <div className="fixed w-[15%] bg-white rounded p-3 shadow">
@@ -22,7 +46,7 @@ export default function ReservationCard() {
       <div className="my-3 flex flex-col">
         <label htmlFor="">Party size</label>
         <select name="" className="py-3 border-b font-light" id="">
-          {partySize.map(size => (
+          {partySize.map((size) => (
             <option value={size.value}>{size.label}</option>
           ))}
         </select>
@@ -30,23 +54,30 @@ export default function ReservationCard() {
       <div className="flex justify-between">
         <div className="flex flex-col w-[48%]">
           <label htmlFor="">Date</label>
-          <DatePicker selected={selectedDate} onChange={handleChangeDate} className="py-3 border-b font-light text-reg w-24" dateFormat="MMMM d" wrapperClassName="w-[48%]"/>
+          <DatePicker
+            selected={selectedDate}
+            onChange={handleChangeDate}
+            className="py-3 border-b font-light text-reg w-24"
+            dateFormat="MMMM d"
+            wrapperClassName="w-[48%]"
+          />
         </div>
         <div className="flex flex-col w-[48%]">
           <label htmlFor="">Time</label>
           <select name="" id="" className="py-3 border-b font-light">
-            <option value="">7:30 AM</option>
-            <option value="">9:30 AM</option>
+            {filterTimeByRestaurantOpenWindow().map((time) => (
+              <option value={time.time}>
+                {time.displayTime}
+              </option>
+            ))}
           </select>
         </div>
       </div>
       <div className="mt-5">
-        <button
-          className="bg-red-600 rounded w-full px-4 text-white font-bold h-16"
-        >
+        <button className="bg-red-600 rounded w-full px-4 text-white font-bold h-16">
           Find a Time
         </button>
       </div>
     </div>
-  )
+  );
 }
--- app/restaurant/[slug]/page.tsx
+++ app/restaurant/[slug]/page.tsx
@@ -15,6 +15,8 @@ interface Restaurant {
   name: string;
   images: string[];
   description: string;
+  open_time: string;
+  close_time: string;
   slug: string;
   reviews: Review[];
 }
@@ -30,7 +32,9 @@ const fetchRestaurantBySlug = async (slug: string): Promise<Restaurant> => {
       images: true,
       description: true,
       slug: true,
-      reviews: true
+      reviews: true,
+      open_time: true,
+      close_time: true,
     }
   });
 
@@ -54,7 +58,10 @@ export default async function RestaurantDetails({params}: {params: {slug: string
       <Reviews reviews={restaurant.reviews}/>
     </div>
     <div className="w-[27%] relative text-reg">
-      <ReservationCard />
+      <ReservationCard 
+        openTime={restaurant.open_time} 
+        closeTime={restaurant.close_time}
+      />
     </div>
   </>
   )
