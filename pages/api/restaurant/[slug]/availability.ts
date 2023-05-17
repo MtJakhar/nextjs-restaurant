91d512a4cfe780b0a63762845b0e35022ce46827
MtJakhar <thvsd.10fingers@gmail.com>
2023-05-17 11:46:23 -0400

    uploaded with type error

--- pages/api/restaurant/[slug]/availability.ts
+++ pages/api/restaurant/[slug]/availability.ts
@@ -1,4 +1,5 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { times } from "../../../../data"
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse){ 
   const {slug, day, time, partySize} = req.query as {
@@ -14,7 +15,17 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     })
   }
 
-  return res.json({slug, day, time, partySize})
+  const searchTimes = times.find(t => {
+    return t.time === time
+  })?.searchTimes;
+
+  if(!searchTimes){
+    return res.status(400).json({
+      errorMessage: "Invalid data provided"
+    })
+  }
+
+  return res.json({searchTimes})
 }
 
 // http://localhost:3000/api/restaurant/vivaan-fine-indian-cuisine-ottawa/availability?day=2023-01&time=20:00:00.000Z&partySize=4
