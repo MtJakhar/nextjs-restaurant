8675ff85aa3ad9e3d707c0c9018c235069de6151
MtJakhar <thvsd.10fingers@gmail.com>
2023-05-19 15:38:18 -0400

    added tables data to availability

--- pages/api/restaurant/[slug]/availability.ts
+++ pages/api/restaurant/[slug]/availability.ts
@@ -1,5 +1,8 @@
+import { Prisma, PrismaClient } from "@prisma/client";
 import { NextApiRequest, NextApiResponse } from "next";
 import { times } from "../../../../data"
+
+const prisma = new PrismaClient()
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse){ 
   const {slug, day, time, partySize} = req.query as {
@@ -25,7 +28,68 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     })
   }
 
-  return res.json({searchTimes})
+  const bookings = await prisma.booking.findMany({
+    where: {
+      booking_time: {
+        gte: new Date(`${day}T${searchTimes[0]}`),
+        lte: new Date(`${day}T${searchTimes[searchTimes.length - 1]}`),
+      }
+    },
+    select: {
+      number_of_people: true,
+      booking_time: true,
+      tables: true
+    }
+  })
+
+  const bookingTablesObj : { [key: string]: {[key: number]: true} } = {};
+
+  bookings.forEach(booking => {
+    bookingTablesObj[booking.booking_time.toISOString()] = booking.tables.reduce((obj, table) => {
+      return {
+        ...obj,
+        [table.table_id]: true
+      }
+    }, {})
+  })
+
+  const restaurant = await prisma.restaurant.findUnique({
+    where: {
+      slug
+    },
+    select: {
+      tables: true
+    }
+  });
+
+  if(!restaurant){
+    return res.status(400).json({
+      errorMessage: "Invalid data provided"
+    })
+  }
+
+  const tables = restaurant.tables
+
+  const searchTimesWithTables = searchTimes.map(searchTime => {
+    return {
+      date: new Date(`${day}T${searchTime}`),
+      time: searchTime,
+      tables
+    }
+  })
+
+  searchTimesWithTables.forEach(t => {
+    t.tables = t.tables.filter(table => {
+      if(bookingTablesObj[t.date.toISOString()]){
+        if(bookingTablesObj[t.date.toISOString()][table.id]) return false
+      }
+      return true
+    })
+  })
+
+  return res.json({searchTimes, bookings, bookingTablesObj, tables, searchTimesWithTables})
 }
 
-// http://localhost:3000/api/restaurant/vivaan-fine-indian-cuisine-ottawa/availability?day=2023-01&time=20:00:00.000Z&partySize=4
+// http://localhost:3000/api/restaurant/vivaan-fine-indian-cuisine-ottawa/availability?day=2023-05-27&time=14:00:00.000Z&partySize=4
+// 2023-05-27T14:00:00.000Z
+// vivaan-fine-indian-cuisine-ottawa
--- pages/api/seed.ts
+++ pages/api/seed.ts
@@ -11,7 +11,7 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  // await prisma.table.deleteMany();
+  await prisma.table.deleteMany();
   await prisma.review.deleteMany();
   await prisma.item.deleteMany();
   await prisma.restaurant.deleteMany();
