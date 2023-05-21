a861cceced0cdb6e0d0bf3adae2c537c93718a9a
MtJakhar <thvsd.10fingers@gmail.com>
2023-05-21 15:56:56 -0400

    Finished!!!!

--- app/reserve/[slug]/page.tsx
+++ app/reserve/[slug]/page.tsx
@@ -1,7 +1,7 @@
-import Header from "./components/Header";
-import Form from "./components/Form";
 import { PrismaClient } from "@prisma/client";
 import { notFound } from "next/navigation";
+import Header from "./components/Header";
+import Form from "./components/Form";
 
 const prisma = new PrismaClient();
 
@@ -20,17 +20,26 @@ const fetchRestaurantBySlug = async (slug: string) => {
 
 export default async function Reserve({
   params,
-  searchParams
+  searchParams,
 }: {
   params: { slug: string };
-  searchParams: {date: string; partySize: string};
+  searchParams: { date: string; partySize: string };
 }) {
   const restaurant = await fetchRestaurantBySlug(params.slug);
   return (
     <div className="border-t h-screen">
       <div className="py-9 w-3/5 m-auto">
-        <Header image={restaurant.main_image} name={restaurant.name} date={searchParams.date} partySize={searchParams.partySize}/>
-        <Form />
+        <Header
+          image={restaurant.main_image}
+          name={restaurant.name}
+          date={searchParams.date}
+          partySize={searchParams.partySize}
+        />
+         <Form
+          partySize={searchParams.partySize}
+          slug={params.slug}
+          date={searchParams.date}
+        />
       </div>
     </div>
   );
